015442322e845d1cde0949090872a34bde03befb
Levon Martirosyan (lev) <levmartiros@yandex.ru>
2021-04-06 11:15:55 +0400

    added oidp resource

--- modules/aws-load-balancer-controller/variables.tf
+++ modules/aws-load-balancer-controller/variables.tf
@@ -1,3 +1,8 @@
+variable "cluster_oidc_issuer" {
+  type = string
+  description = "Cluster identity oidc issuer. Needed for tls_cert generation and oidc provider connection."
+}
+
 variable "cluster_name" {
   type        = string
   default     = ""
@@ -14,4 +19,14 @@ variable "create_namespace" {
   type        = bool
   default     = false
   description = "wether or no to create namespace"
+}
+
+variable "oidc_thumbprint_list" {
+  type    = list(string)
+  default = []
+}
+
+variable "iam_policy_json" {
+  type = string
+  default = "iam-policy.json"
 }
