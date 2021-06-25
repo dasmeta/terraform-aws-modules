f14a54287a99e79f2b97934f1e295c8313b22e28
Aram Karapetyan <aram@dasmeta.com>
2021-06-25 18:55:59 +0400

    Expose access to k8s cluster and create module for mongodb

---
+++ modules/mongodb-replicaset/variables.tf
@@ -0,0 +1,17 @@
+variable setup {
+  type        = string
+  default     = "standalone"
+  description = "Which mongodb setup to consider: standalone (default), replicaset."
+}
+
+variable "cluster_host" {
+  type = string
+}
+
+variable "cluster_certificate" {
+  type = string
+}
+
+variable "cluster_token" {
+  type = string
+}
