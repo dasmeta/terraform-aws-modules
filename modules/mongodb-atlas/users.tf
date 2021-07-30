66b351907392996234cd716e7a8c0f926d3f3aee
Aram Karapetyan <aram@dasmeta.com>
2021-07-30 19:01:56 +0400

    Readme, how to use mongodb-atlas module

--- modules/mongodb-atlas/users.tf
+++ modules/mongodb-atlas/users.tf
@@ -1,49 +1,49 @@
-resource "random_password" "password" {
-  for_each = toset( var.users )
+# resource "random_password" "password" {
+#   for_each = toset( var.users )
   
-  length           = 16
-  special          = true
-  override_special = "_%@"
-}
+#   length           = 16
+#   special          = true
+#   override_special = "_%@"
+# }
 
-resource "mongodbatlas_database_user" "test" {
-  for_each = toset(var.users)
+# resource "mongodbatlas_database_user" "test" {
+#   for_each = toset(var.users)
 
-  username           = each.key
-  password           = random_password.password[count.index]
+#   username           = each.key
+#   password           = random_password.password[count.index]
 
-  project_id         = "<PROJECT-ID>"
-  auth_database_name = "admin"
+#   project_id         = "<PROJECT-ID>"
+#   auth_database_name = "admin"
 
-  roles {
-    role_name     = "readWrite"
-    database_name = "dbforApp"
-  }
+#   roles {
+#     role_name     = "readWrite"
+#     database_name = "dbforApp"
+#   }
 
-  roles {
-    role_name     = "readAnyDatabase"
-    database_name = "admin"
-  }
+#   roles {
+#     role_name     = "readAnyDatabase"
+#     database_name = "admin"
+#   }
 
-  labels {
-    key   = "My Key"
-    value = "My Value"
-  }
+#   labels {
+#     key   = "My Key"
+#     value = "My Value"
+#   }
 
-  scopes {
-    name   = "My cluster name"
-    type = "CLUSTER"
-  }
+#   scopes {
+#     name   = "My cluster name"
+#     type = "CLUSTER"
+#   }
 
-  scopes {
-    name   = "My second cluster name"
-    type = "CLUSTER"
-  }
-}
+#   scopes {
+#     name   = "My second cluster name"
+#     type = "CLUSTER"
+#   }
+# }
 
-output users {
-  value       = ""
-  sensitive   = true
-  description = "description"
-  depends_on  = []
-}
+# output users {
+#   value       = ""
+#   sensitive   = true
+#   description = "description"
+#   depends_on  = []
+# }
