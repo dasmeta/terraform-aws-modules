3933c2b94899e40eae3ad5fa78c91104d57ae62b
viktoryathegreat <ghazaryan.victorya@gmail.com>
2021-08-04 17:23:47 +0400

    Changed default values.

--- modules/mongodb-atlas/variables.tf
+++ modules/mongodb-atlas/variables.tf
@@ -15,13 +15,13 @@ variable org_id {
 
 variable project_name {
   type        = string
-  default     = "Project"
+  default     = "project"
   description = "MongoDB Atlas Project Name"
 }
 
 variable users {
   type        = list(string)
-  default     = ["Alice"]
+  default     = ["alice"]
   description = "MongoDB Atlas users list"
 }
 
@@ -33,7 +33,7 @@ variable role_name {
 
 variable database_name {
   type        = string
-  default     = "Database"
+  default     = "database"
   description = "MongoDB Atlas users database name"
 }
 
