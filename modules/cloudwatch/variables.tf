08d2edc4b9ad8afb722dbce89974fe849c0d47f9
viktoryathegreat <ghazaryan.victorya@gmail.com>
2021-08-06 17:09:19 +0400

    Created a new module to create Cloudwatch dashboard

---
+++ modules/cloudwatch/variables.tf
@@ -0,0 +1,10 @@
+variable dashboard_name {
+    type = string
+    default = "newdashboard"
+    description = "Cloudwatch dashboard name"
+}
+
+variable body_file_name {
+    type = string
+    description = "Cloudwatch dashboard body's json file name"
+}
