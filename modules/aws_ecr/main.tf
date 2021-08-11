41083b135bc5fbe00beb369cae9dfc3a94420018
devopsmher <87768341+devopsmher@users.noreply.github.com>
2021-08-11 10:12:32 +0400

    Update main.tf

--- modules/aws_ecr/main.tf
+++ modules/aws_ecr/main.tf
@@ -8,6 +8,6 @@ provider "aws" {
 module "ecr" {
   source  = "cloudposse/ecr/aws"
   version = "0.32.2"
-  for_each = { for x in var.repo : x => x }
+  for_each = { for x in var.repos : x => x }
   name  = "${each.value}"
 }
