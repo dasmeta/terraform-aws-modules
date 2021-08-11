034c4aa4447503902cafb2bd61cb80843719f095
devopsmher <87768341+devopsmher@users.noreply.github.com>
2021-08-11 10:06:37 +0400

    rename it to repos

--- modules/aws_ecr/variables.tf
+++ modules/aws_ecr/variables.tf
@@ -1,4 +1,4 @@
-variable "repo" {
+variable "repos" {
   description = "<b>Create repository list.\nPlease insert the repository names\nexample 1: [\"service1\"]\nexample 2: [\"service1\", \"service2\", \"serviceN\"]\n\n0 out of 256 characters maximum (2 minimum). The name must start with a letter and can only contain lowercase letters, numbers, hyphens, underscores, and forward slashes."
   type = list
 # default     = ["nginx", "jankins", "apache"]
