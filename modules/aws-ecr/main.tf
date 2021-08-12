b195f6300078fb7339db5094dcaa7137fbd5452f
Mher Poghosyan <87768341+devopsmher@users.noreply.github.com>
2021-08-12 17:05:42 +0400

    Create files main.tf and README.md (#17)
    * Create files main.tf and README.md
    
    * added a blank line

---
+++ modules/aws-ecr/main.tf
@@ -0,0 +1,12 @@
+module "ecr" {
+  source  = "cloudposse/ecr/aws"
+  version = "0.32.2"
+  for_each = { for x in var.repos : x => x }
+  name  = "${each.value}"
+}
+variable "repos" {
+  description = "0 out of 256 characters maximum (2 minimum). The name must start with a letter and can only contain lowercase letters, numbers, hyphens, underscores, and forward slashes."
+  type = list
+  default     = ["repo1", "repo2", "repo3"] # Please insert the repository names, then run the script.
+}
+  
