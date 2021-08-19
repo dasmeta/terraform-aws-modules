cf3a7418ebea614da9e108a44b257d2a2517b8a8
Mher Poghosyan <devops.mher@gmail.com>
2021-08-19 14:23:49 +0400

    change region

--- modules/fluent-bit/main.tf
+++ modules/fluent-bit/main.tf
@@ -39,6 +39,7 @@ POLICY
 locals {
   fluent_name = var.fluent_bit_name != "" ? var.fluent_bit_name : "${var.cluster_name}-fluent-bit"
   log_group_name = var.log_group_name != "" ? var.log_group_name : "fluent-bit-cloudwatch"
+  region = var.region
 }
 
 resource "aws_iam_role_policy_attachment" "CloudWatchAgentServerPolicy" {
@@ -55,7 +56,7 @@ resource "helm_release" "fluent-bit" {
 
   values = [
     # file("${path.module}/values.yaml")
-    templatefile("${path.module}/values.yaml", { log_group_name = local.log_group_name})
+    templatefile("${path.module}/values.yaml", { log_group_name = local.log_group_name,  region = local.region })
   ]
 
   set {
