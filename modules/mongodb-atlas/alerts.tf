622e40e67605905503743d507e906962f38bf6f2
viktoryathegreat <ghazaryan.victorya@gmail.com>
2021-08-25 11:53:41 +0400

    Created a MongoDB alert to notify when CPU is above 99%.

--- modules/mongodb-atlas/alerts.tf
+++ modules/mongodb-atlas/alerts.tf
@@ -1,26 +1,22 @@
 resource "mongodbatlas_alert_configuration" "test" {
   project_id = mongodbatlas_project.main.id
-  event_type = "REPLICATION_OPLOG_WINDOW_RUNNING_OUT" //The type of event that will trigger an alert.
-  enabled    = true // It is not required, but If the attribute is omitted, by default will be false, and the configuration would be disabled. You must set true to enable the configuration.
+  event_type = var.alert_event_type
+  enabled    = true
 
   notification {
-    type_name     = "GROUP" //type of alert notification
-    interval_min  = 5 //Number of minutes to wait between successive notifications for unacknowledged alerts that are not resolved. 
-    delay_min     = 0 //Number of minutes to wait after an alert condition is detected before sending out the first notification.
-    sms_enabled   = false //Flag indicating if text message notifications should be sent.
-    email_enabled = true //Flag indicating if email notifications should be sent.
-    roles         = ["GROUP_CLUSTER_MANAGER"] //The following roles grant privileges within a project. Accepted values are:
+    type_name     = var.alert_type_name
+    interval_min  = var.alert_interval_min
+    delay_min     = var.alert_delay_min
+    sms_enabled   = var.alert_sms_enabled
+    email_enabled = var.alert_email_enabled
+    roles         = var.alert_roles
   }
 
-  matcher {
-    field_name = "HOSTNAME_AND_PORT" //Name of the field in the target object to match on.
-    operator   = "EQUALS" // The operator to test the field’s value. 
-    value      = "SECONDARY" //Value to test with the specified operator. 
-  }
-
-  threshold = {
-    operator    = "LESS_THAN" // Operator to apply when checking the current metric value against the threshold value.
-    threshold   = 72 //Threshold value outside of which an alert will be triggered.
-    units       = "HOURS" // The units for the threshold value. Depends on the type of metric.
+  metric_threshold = {
+    metric_name = var.alert_metric_name
+    operator    = var.alert_operator
+    threshold   = var.alert_threshold
+    units       = var.alert_units
+    mode        = var.alert_mode
   }
 }
--- modules/mongodb-atlas/variables.tf
+++ modules/mongodb-atlas/variables.tf
@@ -42,3 +42,75 @@ variable ip_addresses {
   default     = []
   description = "MongoDB Atlas IP Access List"
 }
+
+variable alert_event_type {
+  type        = string
+  default     = "OUTSIDE_METRIC_THRESHOLD"
+  description = "The type of event that will trigger an alert."
+}
+
+variable alert_type_name {
+  type        = string
+  default     = "GROUP"
+  description = "The type of alert notification."
+}
+
+variable alert_interval_min {
+  type        = number
+  default     = 5
+  description = "Number of minutes to wait between successive notifications for unacknowledged alerts that are not resolved."
+}
+
+variable alert_delay_min {
+  type        = number
+  default     = 0
+  description = "Number of minutes to wait after an alert condition is detected before sending out the first notification."
+}
+
+variable alert_sms_enabled {
+  type        = bool
+  default     = false
+  description = "Flag indicating if text message notifications should be sent."
+}
+
+variable alert_email_enabled {
+  type        = bool
+  default     = true
+  description = "Flag indicating if email notifications should be sent."
+}
+
+variable alert_roles {
+  type        = list(string)
+  default     = ["GROUP_CLUSTER_MANAGER", "GROUP_OWNER"]
+  description = "The following roles grant privileges within a project."
+}
+
+variable alert_metric_name {
+  type        = string
+  default     = "NORMALIZED_SYSTEM_CPU_USER"
+  description = "Name of the metric to check."
+}
+
+variable alert_operator {
+  type        = string
+  default     = "GREATER_THAN"
+  description = "Operator to apply when checking the current metric value against the threshold value."
+}
+
+variable alert_threshold {
+  type        = number
+  default     = 99.0
+  description = "Threshold value outside of which an alert will be triggered."
+}
+
+variable alert_units {
+  type        = string
+  default     = "RAW"
+  description = "The units for the threshold value. Depends on the type of metric."
+}
+
+variable alert_mode {
+  type        = string
+  default     = "AVERAGE"
+  description = "This must be set to AVERAGE. Atlas computes the current metric value as an average."
+}
