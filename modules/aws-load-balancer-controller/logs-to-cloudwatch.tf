b73e0103dcb735b5aa6f0cf99d08555394c40024
Mher Poghosyan <87768341+devopsmher@users.noreply.github.com>
2021-09-24 18:38:10 +0400

    update fix ingress (#30)
    * update fix error
    
    * change Logs to cloudwatch
    
    * Remove version as local paths are not supported
    
    * update
    
    * Fix variables
    
    Co-authored-by: Aram Karapetyan <aram@dasmeta.com>

--- modules/aws-load-balancer-controller/logs-to-cloudwatch.tf
+++ modules/aws-load-balancer-controller/logs-to-cloudwatch.tf
@@ -1,69 +1,35 @@
-data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
-
-module "logs-to-cloudwatch" {
-source = "raymondbutcher/lambda-builder/aws"  
-version = "1.1.0"
-
-  function_name = coalesce(var.function_name, "${var.bucket_name}-to-cloudwatch-logs")
-  handler       = "lambda.handler"
-  runtime       = "python3.7"
-  memory_size   = var.memory_size
-  timeout       = var.timeout
-
-  # Build the package from the source directory and write it to the specified
-  # filename. The function has no dependencies and no build steps so this
-  # build mode is suitable; it just zips up the directory into the filename.
-  build_mode = "FILENAME"
-  source_dir = "${path.module}/src"
-  filename   = "${path.module}/package.zip"
-
-  # Create and use an IAM role which can log function output to CloudWatch,
-  # plus the custom policy which can copy ALB logs from S3 to CloudWatch.
-  role_cloudwatch_logs       = true
-  role_custom_policies       = [data.aws_iam_policy_document.lambda.json]
-  role_custom_policies_count = 1
-
-  environment = {
-    variables = {
-      LOG_GROUP_NAME = var.log_group_name
-    }
-  }
+resource "aws_cloudwatch_log_group" "test" {
+  name              = "ingress-${aws_s3_bucket.ingress-logs-bucket.bucket}"
+  retention_in_days = 365
 }
 
-data "aws_iam_policy_document" "lambda" {
-  statement {
-    actions   = ["s3:GetObject"]
-    effect    = "Allow"
-    resources = ["arn:aws:s3:::${var.bucket_name}/*"]
-  }
-  statement {
-    actions   = ["logs:CreateLogStream", "logs:DeleteLogStream", "logs:PutLogEvents"]
-    effect    = "Allow"
-    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${var.log_group_name}:log-stream:*"]
-  }
+module "alb_logs_to_cloudwatch" {
+  source  = "./terraform-aws-alb-cloudwatch-logs-json"
+
+  bucket_name    = aws_s3_bucket.ingress-logs-bucket.bucket
+  log_group_name = aws_cloudwatch_log_group.test.name
+
+  create_alarm  = false
+  # alarm_actions = [aws_sns_topic.slack.arn]
+  # ok_actions    = [aws_sns_topic.slack.arn]
 }
 
-resource "aws_cloudwatch_metric_alarm" "errors" {
-  count = var.create_alarm ? 1 : 0
+resource "aws_lambda_permission" "bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = module.alb_logs_to_cloudwatch.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.ingress-logs-bucket.arn
+}
 
-  alarm_name        = "${module.lambda.function_name}-errors"
-  alarm_description = "${module.lambda.function_name} invocation errors"
+resource "aws_s3_bucket_notification" "logs" {
+  bucket     = aws_s3_bucket.ingress-logs-bucket.bucket
+  depends_on = [
+    aws_lambda_permission.bucket
+  ]
 
-  namespace   = "AWS/Lambda"
-  metric_name = "Errors"
-
-  dimensions = {
-    FunctionName = module.lambda.function_name
+  lambda_function {
+    lambda_function_arn = module.alb_logs_to_cloudwatch.function_arn
+    events              = ["s3:ObjectCreated:*"]
   }
-
-  statistic           = "Sum"
-  comparison_operator = "GreaterThanThreshold"
-  threshold           = 0
-  period              = 60 * 60
-  evaluation_periods  = 1
-
-  alarm_actions = var.alarm_actions
-  ok_actions    = var.ok_actions
 }
