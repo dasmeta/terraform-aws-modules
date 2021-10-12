d34276b6226f4a54d6e45b7cbcba2c2cc3e6647c
Aram Karapetyan <aram@dasmeta.com>
2021-10-12 11:23:09 +0200

    Adjust policy to be able to use slashes in when namespacing secrets

--- modules/external-secret-store/iam-policy.tf
+++ modules/external-secret-store/iam-policy.tf
@@ -18,7 +18,7 @@ resource "aws_iam_policy" "policy" {
           "secretsmanager:ListSecretVersionIds"
         ],
         "Resource": [
-          "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:${var.name}-*",
+          "arn:aws:secretsmanager:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:secret:${var.name}*",
         ]
       }
     ]
