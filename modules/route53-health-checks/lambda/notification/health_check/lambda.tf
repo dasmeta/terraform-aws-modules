91304557c33daed3c1c9482963f340a69d8db616
Mher Poghosyan <devops.mher@gmail.com>
2021-10-20 18:37:49 +0400

    Add new module for route53 healthcheck alert to slack

---
+++ modules/route53-health-checks/lambda/notification/health_check/lambda.tf
@@ -0,0 +1,25 @@
+data "archive_file" "this" {
+  type        = "zip"
+  output_path = "${path.module}/deploy.zip"
+  source_dir = "${path.module}/notify-slack/nodejs"
+}
+
+# Define lambda function to request slack endpoint
+resource "aws_lambda_function" "health_check_slack_notification_lambda" {
+  provider = "aws.virginia"
+
+  function_name = "${replace(var.domen_name, ".", "-")}-health-check-to-slack"
+  filename = "${path.module}/deploy.zip"
+  source_code_hash = data.archive_file.this.output_base64sha256
+  handler = "index.handler"
+  runtime = "nodejs12.x"
+  role = aws_iam_role.iam_for_lambda_health_check.arn
+  memory_size = 1024
+  timeout = 5
+
+  environment {
+    variables = {
+      hook_url = var.slack_hook_url
+    }
+  }
+}
