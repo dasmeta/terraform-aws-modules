87cc12f4fe10f5d7b0d3413fb7871b31922e137e
Mher Poghosyan <devops.mher@gmail.com>
2021-10-22 09:52:41 +0400

    fix function name

--- modules/route53-health-checks/sns_lambda_slack_topic_subscription.tf
+++ modules/route53-health-checks/sns_lambda_slack_topic_subscription.tf
@@ -10,7 +10,7 @@ data "aws_sns_topic" "aws_sns_topic_slack_health_check" {
 }
 
 data "aws_lambda_function" "health_check_slack_notification_lambda" {
-  function_name = "${replace("${var.domen_name}${var.resource_path}", "/[./]+/", "-")}-health-check-to-slack"
+  function_name = "${replace("${var.domen_name}${var.resource_path}", "/[./]+/", "-")}-slack"
   depends_on = [
     module.slack_notification_lambda_health_checks,aws_sns_topic.route53-healthcheck,
   ]
