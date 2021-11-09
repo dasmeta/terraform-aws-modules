c833ca12f0a353e7960a2f3865fa4513d35f1064
Aram Karapetyan <aram@dasmeta.com>
2021-11-09 17:24:03 +0400

    Move to requirements.tf and set minial version as this does not work with 0.14

--- modules/mongodb-atlas/provider.tf
+++ modules/mongodb-atlas/provider.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    mongodbatlas = {
-      source = "mongodb/mongodbatlas"
-      version = "0.9.1"
-    }
-  }
-}
-
 provider "mongodbatlas" {
   public_key = var.public_key
   private_key  = var.private_key
