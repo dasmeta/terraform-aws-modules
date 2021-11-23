7c7a065c4bc6a81d39627f7ad3802a43478c0075
viktoryathegreat <ghazaryan.victorya@gmail.com>
2021-11-23 16:58:25 +0400

    Style fix.

--- modules/cognito-user-pool/group.tf
+++ modules/cognito-user-pool/group.tf
@@ -1,5 +1,6 @@
 resource "aws_cognito_user_group" "group" {
   count        = (var.user_group != "") ? 1 : 0
+  
   name         = var.user_group
   user_pool_id = aws_cognito_user_pool.pool.id
   precedence   = var.precedence
