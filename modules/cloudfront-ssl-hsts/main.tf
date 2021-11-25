aee68841c16317ca174cad097d735cddb1e70278
Mher Poghosyan <devops.mher@gmail.com>
2021-11-25 19:27:22 +0400

    fix bag

--- modules/cloudfront-ssl-hsts/main.tf
+++ modules/cloudfront-ssl-hsts/main.tf
@@ -1,5 +1,4 @@
 locals {
-  
   create_origin_access_identity = var.create_origin_access_identity && length(keys(var.origin_access_identities)) > 0
 }
 
--- modules/cloudfront-ssl-hsts/ssl-certificate.tf
+++ modules/cloudfront-ssl-hsts/ssl-certificate.tf
@@ -1,7 +1,8 @@
 module ssl-certificate-auth {
+  count = var.create_certificate ? 1 : 0
+  
   source = "dasmeta/modules/aws//modules/ssl-certificate"
   version = "0.17.1"
-  count = var.create_certificate ? 1 : 0
   domain              = element(var.aliases, 0)
   alternative_domains = slice (var.aliases,1,length(var.aliases))
   zone                = element(var.zone, 0)
