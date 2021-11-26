5cf0c941fc03c7b832b080d251a21e07a7acda80
aghamyan44 <juliaaghamyan44@gmail.com>
2021-11-26 18:41:00 +0400

    Disk size parameter in eks module

--- modules/eks/variables.tf
+++ modules/eks/variables.tf
@@ -72,9 +72,11 @@ variable "worker_groups" {
 variable "workers_group_defaults" {
   type = object({
     root_volume_type = string
+    root_volume_size = number
   })
   default = {
     root_volume_type = "gp2"
+    root_volume_size = 50
     kubelet_extra_args = "--node-labels=cluster_name=production,type=gpu_optimised --register-with-taints app=vums:NoSchedule"
   }
   description = "Worker group defaults."
