720062f2a2f7dd4b9cb1cbb9c7b47c1bf7389428
Tigran Muradyan <tigran@dasmeta.com>
2022-01-06 19:27:29 +0400

    Have certificate module variable fixes

--- modules/ssl-certificate/variables.tf
+++ modules/ssl-certificate/variables.tf
@@ -6,6 +6,7 @@ variable "domain" {
 variable "alternative_domains" {
   type        = list(string)
   description = "Subdomain or other domain or wildcard certificate name will be create."
+  default = []
 }
 
 variable "zone" {
@@ -13,12 +14,14 @@ variable "zone" {
   description = "This variable use route53. Can equal to main domain name."
 }
 
-variable "alternative_zone" {
+variable "alternative_zones" {
   type        = list(string)
   description = "This variable use route53. Must equal to alternative_domains. (Note. When you use wildcard must be equal to main zone)"
+  default = []
 }
 
 variable "tags" {
-  type        = object({ name = string, value = string })
-  description = "Object Domain and Zone"
+  type        = any
+  description = "tags"
+  default     = {}
 }
