8b7ac04ee971c00f1a76d859dc43ba919f93e2ba
Mher Poghosyan <devops.mher@gmail.com>
2022-03-14 13:26:18 +0400

    fix aws version in complete-eks-cluser module

--- modules/complete-eks-cluster/providers.tf
+++ modules/complete-eks-cluster/providers.tf
@@ -4,12 +4,12 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.31"
+      version = "=> 3.31"
     }
 
     helm = {
-      source = "hashicorp/helm"
-      version = "~> 2.2.0"
+      source  = "hashicorp/helm"
+      version = "=> 2.2.0"
     }
   }
 }
