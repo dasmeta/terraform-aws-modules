430885316b807526dd043e6be0678e64f8cd4cc5
Tigran Muradyan <tigran@dasmeta.com>
2022-03-22 16:25:51 +0400

    set incremental version for lmbda security headers

--- modules/cloudfront-ssl-hsts/lamda-security-headers.tf
+++ modules/cloudfront-ssl-hsts/lamda-security-headers.tf
@@ -2,7 +2,7 @@ module "aws-cloudfront-security-headers" {
   count = var.create_hsts ? 1 : 0
 
   source  = "dasmeta/modules/aws//modules/aws-cloudfront-security-headers"
-  version = "0.22.6"
+  version = "0.23.1"
 
   name                    = "${substr(replace(var.aliases[0], ".", "-"), 0, 32)}-security-headers"
   override_custom_headers = var.override_custom_headers
