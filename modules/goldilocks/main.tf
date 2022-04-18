b05f183a47923cf5c71b7cf3e337cc2750009efb
aghamyan44 <juliaaghamyan44@gmail.com>
2022-04-18 17:39:49 +0400

    Fixs Source

--- modules/goldilocks/main.tf
+++ modules/goldilocks/main.tf
@@ -58,7 +58,7 @@ resource "helm_release" "goldilocks_deploy" {
 
 module "ingress" {
   count  = var.create_dashboard_ingress ? 1 : 0
-  source = "dasmeta/modules/aws//modules/goldilocks/ingress"
+  source = "dasmeta/modules/aws//modules/ingress"
 
   alb_name    = var.alb_name
   hostname    = var.hostname
@@ -82,8 +82,8 @@ module "ingress" {
     "alb.ingress.kubernetes.io/success-codes"      = "200-399"
     "alb.ingress.kubernetes.io/certificate-arn"    = var.alb_certificate_arn
     "alb.ingress.kubernetes.io/auth-type"          = "cognito"
-    "alb.ingress.kubernetes.io/auth-idp-cognito" = local.auth
-    "alb.ingress.kubernetes.io/listen-ports"     = "[{\"HTTPS\":443}]"
+    "alb.ingress.kubernetes.io/auth-idp-cognito"   = local.auth
+    "alb.ingress.kubernetes.io/listen-ports"       = "[{\"HTTPS\":443}]"
   }
   depends_on = [
     kubernetes_manifest.create_namespace
