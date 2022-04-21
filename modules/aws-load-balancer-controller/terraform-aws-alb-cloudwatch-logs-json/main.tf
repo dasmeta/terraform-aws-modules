3fd3e25ea7ad3fd0f3f93dacfcf8fd5dc8a9ccb2
viktoryathegreat <ghazaryan.victorya@gmail.com>
2022-04-21 11:11:46 +0400

    DMVP-629 Brought the old value back.

--- modules/aws-load-balancer-controller/terraform-aws-alb-cloudwatch-logs-json/main.tf
+++ modules/aws-load-balancer-controller/terraform-aws-alb-cloudwatch-logs-json/main.tf
@@ -6,7 +6,7 @@ module "lambda" {
   source  = "raymondbutcher/lambda-builder/aws"
   version = "1.1.0"
 
-  function_name = coalesce(var.function_name, "${var.bucket_name}")
+  function_name = coalesce(var.function_name, "${var.bucket_name}-logs-to-cloudwatch")
   handler       = "lambda.handler"
   runtime       = "python3.7"
   memory_size   = var.memory_size
