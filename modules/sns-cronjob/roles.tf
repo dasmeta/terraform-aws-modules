f8e6d39ff6616a6577a4566bd722dc433fe13fdd
Tigran Muradyan <tigran@dasmeta.com>
2022-04-21 13:07:30 +0400

    DMVP-558 refactor/fix aws sns-cronjob module

--- modules/sns-cronjob/roles.tf
+++ modules/sns-cronjob/roles.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "success_logger" {
+resource "aws_iam_role" "logger" {
   name = "${var.name}-cron-success-logger"
 
   assume_role_policy = <<EOL
@@ -41,45 +41,62 @@ EOL
   }
 }
 
-resource "aws_iam_role" "failure_logger" {
-  name = "${var.name}-cron-failure-logger"
+data "aws_iam_policy_document" "sns_topic_policy" {
+  policy_id = "${var.name}-cron-sns-policy-id"
 
-  assume_role_policy = <<EOL
-{
-  "Version": "2012-10-17",
-  "Statement": [
-      {
-          "Effect": "Allow",
-          "Principal": {
-              "Service": "sns.amazonaws.com"
-          },
-          "Action": "sts:AssumeRole"
-      }
-  ]
-}
-EOL
+  statement {
+    sid    = "${var.name}-cron-sns-statement-id"
+    effect = "Allow"
 
-  inline_policy {
-    name   = "${var.name}-cron-failure-logger"
-    policy = <<EOL
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "logs:CreateLogGroup",
-                "logs:CreateLogStream",
-                "logs:PutLogEvents",
-                "logs:PutMetricFilter",
-                "logs:PutRetentionPolicy"
-            ],
-            "Resource": [
-                "*"
-            ]
-        }
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
     ]
-}
-EOL
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = ["${aws_sns_topic.this.arn}"]
   }
+
+  statement {
+    sid     = "${var.name}-cron-sns-permission-upper-policy"
+    effect  = "Allow"
+    actions = ["SNS:Publish"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    resources = ["${aws_sns_topic.this.arn}"]
+  }
+
+  statement {
+    sid     = "${var.name}-cron-sns-permission-policy"
+    effect  = "Allow"
+    actions = ["sns:Publish"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["events.amazonaws.com"]
+    }
+
+    resources = ["${aws_sns_topic.this.arn}"]
+  }
+}
+
+resource "aws_sns_topic_policy" "cw_event_permissions" {
+  arn = aws_sns_topic.this.arn
+
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
 }
