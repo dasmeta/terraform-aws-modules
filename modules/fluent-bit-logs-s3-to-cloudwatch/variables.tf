3aa5b64106042a39aca3e99e9983dcdfaf96038f
aghamyan44 <78356417+aghamyan44@users.noreply.github.com>
2022-06-10 13:47:12 +0400

    Dmvp 742 v2 (#175)
    * Fluent-bit-s3-to-cloudwatch
    
    * Change README
    
    * Delete
    
    * Changes
    
    * Add Variable for create s3 bucket
    
    * Change README.md
    
    * DMVP-742: small adjustments
    
    * DMVP-742: set dependencies
    
    Co-authored-by: Karapetyan, Aram (ext.) <aram.karapetyan@proalpha.com>

---
+++ modules/fluent-bit-logs-s3-to-cloudwatch/variables.tf
@@ -0,0 +1,20 @@
+variable "create_lambda_s3_to_cloudwatch" {
+  type    = bool
+  default = true
+}
+
+variable "bucket_name" {
+  type    = string
+  default = "test-fluent-bit-bla"
+}
+
+variable "create_bucket" {
+  type    = bool
+  default = true
+}
+
+variable "assume_role_arn" {
+  type        = list(string)
+  description = "AWS Acounts Assume roles arn which access bucket write"
+  default     = ["arn:aws:iam::*:role/eks-cluster-fluent-bit-role"]
+}
