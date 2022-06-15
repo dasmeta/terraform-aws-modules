83ec8d8c10478dcbc1bad3cf710e087d19a1230a
aghamyan44 <juliaaghamyan44@gmail.com>
2022-06-15 18:36:02 +0400

    Revert "Add namespace"
    This reverts commit d122d81bb5a22d186a08d96c12ce3ca1fbf6fc65.

--- modules/external-secret-store/store.tf
+++ modules/external-secret-store/store.tf
@@ -1,7 +1,6 @@
 resource "kubectl_manifest" "main" {
   yaml_body = templatefile("${path.module}/secret-store.tmpl", {
     name       = local.sanitized-name
-    namespace  = var.namespace
     region     = data.aws_region.current.name
     controller = var.controller
   })
