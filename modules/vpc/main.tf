664de2fe6c771233e509511fe7bc730848791b5f
0katrinpetrosyan0 <katrinpetrosyan321@gmail.com>
2022-07-07 14:00:59 +0400

    check

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.77.0"
+  source   = "terraform-aws-modules/vpc/aws"
+  version  = "2.77.0"
 
   count                = var.create_vpc ? 1 : 0
   name                 = var.vpc_name
