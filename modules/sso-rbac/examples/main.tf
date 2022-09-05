5b347a5c102776dd1e53c7c67a9ec9ab62282493
Vahagn Martirosyan <mianv@coursede.local>
2022-09-05 23:58:52 +0400

    Ignoring .idea dir

--- modules/sso-rbac/examples/main.tf
+++ modules/sso-rbac/examples/main.tf
@@ -4,28 +4,28 @@ module "sso_module" {
   eks_module = module.eks
 
   assignment = [{
-    name = "role1"
-    group = "development"
+    name      = "role1"
+    group     = "development"
     namespace = "development"
-    role = [local.role1]
+    role      = [local.role1]
 
-  },{
-    name = "role2"
-    group = "accounting"
+    }, {
+    name      = "role2"
+    group     = "accounting"
     namespace = "accounting"
-    role = [local.role2]
+    role      = [local.role2]
   }]
 }
 
 locals {
 
   role1 = {
-    actions = ["get", "list", "watch"]
+    actions   = ["get", "list", "watch"]
     resources = ["deployments"]
   }
 
   role2 = {
-    actions = ["get", "list", "watch"]
+    actions   = ["get", "list", "watch"]
     resources = ["pods"]
   }
 }
