aacd9b81a3f390a51ba7872f8b9ee533c2ebff9a
viktoryathegreat <ghazaryan.victorya@gmail.com>
2022-09-14 17:52:53 +0400

    DMVP-937 Module refactoring.

--- modules/mongodb/variables.tf
+++ modules/mongodb/variables.tf
@@ -1,41 +1,9 @@
-variable "setup" {
-  type        = string
-  default     = "standalone"
-  description = "Which mongodb setup to consider: standalone (default), replicaset."
-}
-
-variable "cluster_host" {
-  type = string
-}
-
-variable "cluster_certificate" {
-  type = string
-}
-
-variable "cluster_token" {
-  type = string
-}
-
 variable "name" {
   type        = string
   default     = "mongodb"
   description = "Release name."
 }
 
-variable "values" {
-  description = "Extra values"
-  type        = list(string)
-  default     = null
-}
-
-variable "set" {
-  description = "Value block with custom STRING values to be merged with the values yaml."
-  type = list(object({
-    name  = string
-    value = string
-  }))
-  default = null
-}
 variable "root_password" {
   type = string
 }
