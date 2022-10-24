7601f71796d24711662446f9c8634c4f9739f038
VahagnMian <vahagn.m@dasmeta.com>
2022-10-24 11:34:46 +0400

    feat(DMVP-1338): Modified variable name from zone to zones

--- modules/cloudfront-ssl-hsts/ssl-certificate.tf
+++ modules/cloudfront-ssl-hsts/ssl-certificate.tf
@@ -2,10 +2,10 @@ module "ssl-certificate-auth" {
   count = var.create_certificate ? 1 : 0
 
   source              = "dasmeta/modules/aws//modules/ssl-certificate"
-  version             = "0.17.1"
+  version             = "1.1.2"
   domain              = element(var.aliases, 0)
   alternative_domains = slice(var.aliases, 1, length(var.aliases))
   zone                = element(var.zone, 0)
-  alternative_zone    = slice(var.zone, 1, length(var.zone))
+  alternative_zones    = slice(var.zone, 1, length(var.zone))
   tags                = var.tags
 }
