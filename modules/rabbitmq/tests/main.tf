fa588b2e36aa32f09d375dd26d94f5fccdc25aa5
viktoryathegreat <ghazaryan.victorya@gmail.com>
2022-11-04 11:35:52 +0400

    refactor(DMVP-1382): Added tests foolder.

---
+++ modules/rabbitmq/tests/main.tf
@@ -0,0 +1,12 @@
+module "modules_rabbitmq" {
+  source = "dasmeta/modules/aws//modules/rabbitmq"
+
+  broker_name        = "app-dev-mq"
+  subnet_ids         = ["subnet-231dadsa344ds", "subnet-231dqweqsa344ds", "subnet-241dadsa344ds"]
+  security_groups    = ["sg-asff234adasdd"]
+  username           = "user-terraform"
+  password           = "password@#$23da"
+  engine_version     = "3.8.27"
+  deployment_mode    = "SINGLE_INSTANCE"
+  host_instance_type = "mq.t3.micro"
+}
