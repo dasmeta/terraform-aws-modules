64881e9c0723ebec8c6042bfb30dac98b3b4fd2a
Katrin <katrin@DasMetas-MacBook-Pro.local>
2023-02-08 20:36:13 +0400

    do configurable

--- modules/cognito-user-pool/variables.tf
+++ modules/cognito-user-pool/variables.tf
@@ -331,5 +331,5 @@ variable "create_route53_record" {
 variable "allow_admin_create_user_only" {
   type        = bool
   default     = false
-  description = "Allow admin to create only user"
+  description = "(Optional) Set to True if only the administrator is allowed to create user profiles. Set to False if users can sign themselves up via an app."
 }
