14e6c3260edaee7100b55fe2f69beefa96dca259
Katrin Petrosyan <97396417+0katrinpetrosyan0@users.noreply.github.com>
2023-02-24 11:38:24 +0400

    fix(DMVP-1806): delete kms key

--- modules/api-gateway/cloudwatch.tf
+++ modules/api-gateway/cloudwatch.tf
@@ -3,5 +3,4 @@ resource "aws_cloudwatch_log_group" "access_logs" {
 
   name              = "api-gateway-${var.name}-${var.stage_name}-logs"
   retention_in_days = var.retention_in_days
-  kms_key_id        = var.kms_key_id
 }
