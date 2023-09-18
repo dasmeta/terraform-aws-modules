d3ceac63ca1524180dc8d1e463bbd1e5caf0a8c2
Julieta Aghamyan <julia@dasmeta.com>
2023-09-18 12:18:54 +0400

    fix(DMVP-2690): Added kms key support

--- modules/secret/secret.tf
+++ modules/secret/secret.tf
@@ -1,6 +1,7 @@
 resource "aws_secretsmanager_secret" "secret" {
   name                    = var.name
   recovery_window_in_days = var.recovery_window_in_days
+  kms_key_id              = var.kms_key_id
 }
 
 resource "aws_secretsmanager_secret_version" "value" {
@@ -8,5 +9,4 @@ resource "aws_secretsmanager_secret_version" "value" {
 
   secret_id     = aws_secretsmanager_secret.secret.id
   secret_string = jsonencode(var.value)
-  kms_key_id    = var.kms_key_id
 }
