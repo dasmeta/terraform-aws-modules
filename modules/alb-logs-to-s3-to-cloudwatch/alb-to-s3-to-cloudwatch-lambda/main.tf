716ace8de5fa4bda1c7b293cc8c57e7621f348db
Oleg Volpres <102795468+NJ3rsey@users.noreply.github.com>
2023-10-18 11:31:47 +0300

    Changed python3.7 to 3.11

--- modules/alb-logs-to-s3-to-cloudwatch/alb-to-s3-to-cloudwatch-lambda/main.tf
+++ modules/alb-logs-to-s3-to-cloudwatch/alb-to-s3-to-cloudwatch-lambda/main.tf
@@ -8,7 +8,7 @@ module "lambda" {
 
   function_name = coalesce(var.function_name, "${substr(var.bucket_name, 0, 45)}-to-cloudwatch-logs")
   handler       = "lambda.handler"
-  runtime       = "python3.7"
+  runtime       = "python3.11"
   memory_size   = var.memory_size
   timeout       = var.timeout
 
