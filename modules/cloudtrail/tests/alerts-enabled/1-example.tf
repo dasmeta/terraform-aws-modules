3fcfe19103eb85f851ed6ebafec5d40ae643f030
viktoryathegreat <ghazaryan.victorya@gmail.com>
2024-05-15 09:04:34 +0400

    fix(DMVP-3900): Made cloudtrail alerts insufficient data filled with 0

--- modules/cloudtrail/tests/alerts-enabled/1-example.tf
+++ modules/cloudtrail/tests/alerts-enabled/1-example.tf
@@ -1,10 +1,10 @@
 module "this" {
   source = "../../"
 
-  name = "audit-project"
+  name = "audit-project-test"
 
   enable_cloudwatch_logs      = true
-  cloud_watch_logs_group_name = "audit-project-cloudtrail-logs"
+  cloud_watch_logs_group_name = "audit-project-cloudtrail-logs-test"
 
   alerts = {
     events = ["iam-user-creation-or-deletion"]
--- modules/cloudtrail/tests/alerts-enabled/2-assert.tf
+++ modules/cloudtrail/tests/alerts-enabled/2-assert.tf
@@ -1,9 +1,9 @@
-resource "test_assertions" "dummy" {
-  component = "this"
+# resource "test_assertions" "dummy" {
+#   component = "this"
 
-  equal "scheme" {
-    description = "As module does not have any output and data just make sure the case runs. Probably can be thrown away."
-    got         = "all good"
-    want        = "all good"
-  }
-}
+#   equal "scheme" {
+#     description = "As module does not have any output and data just make sure the case runs. Probably can be thrown away."
+#     got         = "all good"
+#     want        = "all good"
+#   }
+# }
