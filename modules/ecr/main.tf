4fa155fede05ff5998c851427eebef3459648ead
Tigran Muradyan <tigran@dasmeta.com>
2024-08-09 14:29:57 +0400

    fix(DMVP-4914): fix comment

--- modules/ecr/main.tf
+++ modules/ecr/main.tf
@@ -1,6 +1,6 @@
 module "ecr" {
   source = "git::https://github.com/dasmeta/terraform-aws-ecr.git?ref=main"
-  # TODO: we module with direct github repo source because there was need some ability, please check PR: https://github.com/cloudposse/terraform-aws-ecr/issues/133 and uncomment source to tf registry
+  # TODO: we have module with direct github repo source now because there was need protected tags wildcard match ability. please check PR: https://github.com/cloudposse/terraform-aws-ecr/issues/133 and uncomment source/version to the original module tf registry
   # source  = "cloudposse/ecr/aws"
   # version = "0.41.1"
 
