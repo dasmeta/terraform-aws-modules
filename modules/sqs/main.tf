2333ada4ed15b665fc72977a79f62d2424e9f930
Julieta Aghamyan <julia@dasmeta.com>
2025-02-26 11:38:58 +0400

    fix(DMVP-6854): Update SQS module

--- modules/sqs/main.tf
+++ modules/sqs/main.tf
@@ -7,12 +7,12 @@ module "sqs" {
   name            = local.queue_name
   use_name_prefix = var.use_name_prefix
 
-  visibility_timeout_seconds = var.visibility_timeout_seconds
-  message_retention_seconds  = var.message_retention_seconds
-  max_message_size           = var.max_message_size
-  delay_seconds              = var.delay_seconds
-  receive_wait_time_seconds  = var.receive_wait_time_seconds
-  # policy                      = var.policy
+  visibility_timeout_seconds  = var.visibility_timeout_seconds
+  message_retention_seconds   = var.message_retention_seconds
+  max_message_size            = var.max_message_size
+  delay_seconds               = var.delay_seconds
+  receive_wait_time_seconds   = var.receive_wait_time_seconds
+  queue_policy_statements     = var.policy
   redrive_policy              = var.redrive_policy
   redrive_allow_policy        = var.redrive_allow_policy
   fifo_queue                  = var.fifo_queue
