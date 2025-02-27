84e89576c8a4e061bc832351490bee3ad907d3ff
Tigran Muradyan <tigran@dasmeta.com>
2025-02-27 19:48:57 +0400

    fix(DMVP-6703): have aws secret-reader module outputs sensitive

--- modules/cloudfront-ssl-hsts/tests/s3-and-alb/README.md
+++ modules/cloudfront-ssl-hsts/tests/s3-and-alb/README.md
@@ -11,7 +11,7 @@
 
 | Name | Version |
 |------|---------|
-| <a name="provider_aws"></a> [aws](#provider\_aws) | ~> 5.0 |
+| <a name="provider_aws"></a> [aws](#provider\_aws) | 4.67.0 |
 
 ## Modules
 
--- modules/secret-reader/output.tf
+++ modules/secret-reader/output.tf
@@ -1,7 +1,9 @@
 output "secrets" {
-  value = local.secrets
+  value     = local.secrets
+  sensitive = true
 }
 
 output "secret_value" {
-  value = try(local.secrets[var.secret_key], null)
+  value     = try(local.secrets[var.secret_key], null)
+  sensitive = true
 }
