f9ec32c54a3af415d7f68392d6e02e7ebd3e1c67
Julieta Aghamyan <julia@dasmeta.com>
2025-08-05 10:38:53 +0400

    fix(DMVP-fix-cert): Fix certificate validation option , when zone is null code didn't execute

--- modules/cloudfront-ssl-hsts/ssl-certificate.tf
+++ modules/cloudfront-ssl-hsts/ssl-certificate.tf
@@ -1,8 +1,10 @@
 module "ssl-certificate-auth" {
   count = var.create_certificate ? 1 : 0
 
-  source              = "dasmeta/modules/aws//modules/ssl-certificate"
-  version             = "2.18.6"
+  # source              = "dasmeta/modules/aws//modules/ssl-certificate"
+  # version             = "2.18.6"
+
+  source              = "git::https://github.com/dasmeta/terraform-aws-modules.git//modules/ssl-certificate?ref=DMVP-fix-cert"
   domain              = element(var.aliases, 0)
   alternative_domains = slice(var.aliases, 1, length(var.aliases))
   zone                = element(var.zone, 0)
